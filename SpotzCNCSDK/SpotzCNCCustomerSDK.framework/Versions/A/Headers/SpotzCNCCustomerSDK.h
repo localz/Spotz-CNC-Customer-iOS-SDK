98b1f5494f28f15eea453c62390d3291ef082dd8
Melvin <ma@localz.co>
2016-02-22 23:17:52 +1100

    Added delete order

--- SpotzCNCSDK/SpotzCNCCustomerSDK.framework/Versions/A/Headers/SpotzCNCCustomerSDK.h
+++ SpotzCNCSDK/SpotzCNCCustomerSDK.framework/Versions/A/Headers/SpotzCNCCustomerSDK.h
@@ -64,6 +64,7 @@ typedef enum {
 - (void)checkinOrder:(SpotzCNCOrder *)order force:(BOOL)force completion:(void(^)(NSNumber *numOrdersCheckedIn, NSError *error))completion;
 - (void)giveFeedbackComment:(NSString *)feedbackComment responsiveness:(NSNumber *)responsiveness friendliness:(NSNumber *)friendliness usefulness:(NSNumber *)usefulness satisfaction:(NSNumber *)satisfaction orderNumber:(NSString *)orderNumber completion:(void(^)(NSError *error))completion;
 - (void)createOrder:(SpotzCNCOrder *)order branchId:(NSString *)branchId customerId:(NSString *)customerId completion:(void(^)(NSError *error))completion;
+- (void)deleteOrder:(NSString *)orderNumber customerId:(NSString *)customerId completion:(void(^)(NSError *error))completion;
 
 // Push Configuration
 - (void)applicationDidRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
