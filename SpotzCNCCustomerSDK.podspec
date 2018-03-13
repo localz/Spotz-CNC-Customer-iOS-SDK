34c05dc47091b162caab20e8ad469b2ff8d45602
Daniel Sykes-Turner <dst@localz.co>
2018-03-13 14:32:23 +1100

    Bug fix for order.orderAmount class

--- SpotzCNCCustomerSDK.podspec
+++ SpotzCNCCustomerSDK.podspec
@@ -1,20 +1,20 @@
 Pod::Spec.new do |s|
 	s.name	= 'SpotzCNCCustomerSDK'
-	s.platform = :ios,'7.0'
+	s.platform = :ios,'8.0'
 	s.license = {
 		:type => 'Commercial',
 		:text => <<-LICENSE
-			Copyright 2017 Localz Pty Ltd.
+			Copyright 2018 Localz Pty Ltd.
 			LICENSE
 	}
-	s.version = '1.8.1.1'
+	s.version = '1.8.1.2'
 	s.summary = 'SpotzCNCCustomerSDK'
 	s.homepage = 'http://www.localz.com'
 	s.author = { 
 		'Localz Pty Ltd' => 'info@localz.com' 
 	}
 	s.requires_arc = true
-	s.ios.deployment_target = '7.0'
+	s.ios.deployment_target = '8.0'
 	s.xcconfig = { 
 		'FRAMEWORK_SERCH_PATHS' => '$(inherited)' 
 	}
@@ -25,6 +25,6 @@ Pod::Spec.new do |s|
 	s.frameworks = 'CoreLocation','CoreBluetooth'
 
 	# Please also include:
-	#s.dependency 'https://github.com/localz/Spotz3-iOS-SDK'
-	#s.dependency 'https://github.com/localz/SpotzPushSDK'
+	 # 'https://github.com/localz/Spotz3-iOS-SDK'
+	 # 'https://github.com/localz/SpotzPushSDK'
 end
--- SpotzCNCSDK/SpotzCNCCustomerSDK.framework/Versions/A/Headers/SpotzCNCOrder.h
+++ SpotzCNCSDK/SpotzCNCCustomerSDK.framework/Versions/A/Headers/SpotzCNCOrder.h
@@ -21,7 +21,7 @@ typedef enum {
 @interface SpotzCNCOrder : NSObject<NSCoding>
 @property (nonatomic, readonly) SpotzCNCOrderStatus orderStatus;
 @property (nonatomic, readonly, strong) NSString *deliveryName;
-@property (nonatomic, readonly, strong) NSString *orderAmount;
+@property (nonatomic, readonly, strong) NSNumber *orderAmount;
 @property (nonatomic, readonly, strong) NSDate *orderDate;
 @property (nonatomic, readonly, strong) NSString *orderNumber;
 @property (nonatomic, readonly, strong) NSDate *pickupStart;
@@ -35,7 +35,8 @@ typedef enum {
 @property (nonatomic, readonly, strong) SpotzCNCAttendant *attendant;
 
 - (SpotzCNCOrder *)initWithData:(NSDictionary *)data;
-- (SpotzCNCOrder *)initWithOrderNumber:(NSString *)orderNumber orderDate:(NSDate *)orderDate deliveryName:(NSString *)deliveryName orderStatus:(SpotzCNCOrderStatus)status orderAmount:(NSString *)amount pickupStart:(NSDate *)pickupStart pickupDate:(NSDate *)pickupEnd selectedPickupId:(NSString *)selectedPickupId totalItems:(NSNumber *)totalItems attributes:(NSDictionary *)specific allowCheckinPending:(BOOL)allowCheckinPending allowAutoCheckin:(BOOL)allowAutoCheckin;
+- (SpotzCNCOrder *)initWithOrderNumber:(NSString *)orderNumber orderDate:(NSDate *)orderDate deliveryName:(NSString *)deliveryName orderStatus:(SpotzCNCOrderStatus)status orderAmount:(NSString *)amount pickupStart:(NSDate *)pickupStart pickupDate:(NSDate *)pickupEnd selectedPickupId:(NSString *)selectedPickupId totalItems:(NSNumber *)totalItems attributes:(NSDictionary *)specific allowCheckinPending:(BOOL)allowCheckinPending allowAutoCheckin:(BOOL)allowAutoCheckin __deprecated_msg("orderAmount is no longer supporting NSString class. Use alternative initWithOrderNumber: instead.");
+- (SpotzCNCOrder *)initWithOrderNumber:(NSString *)number date:(NSDate *)date deliveryName:(NSString *)deliveryName status:(SpotzCNCOrderStatus)status amount:(NSNumber *)amount pickupStartDate:(NSDate *)start pickupEndDate:(NSDate *)end selectedPickupId:(NSString *)pickupId totalItems:(NSNumber *)totalItems attributes:(NSDictionary *)attributes allowCheckinPending:(BOOL)allowCheckinPending allowAutoCheckin:(BOOL)allowAutoCheckin;
 - (BOOL) isWithinPickupWindow;
 - (BOOL) isTimeToNotify;
 @end
